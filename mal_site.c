16d1f296369957d1302d0bac9294314845baa136
zxchs00 <zxchs00@gmail.com>
2016-08-29 20:22:27 +0900

    initialize

---
+++ mal_site.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pcap.h"
+#pragma comment(lib,"ws2_32.lib")
+
+#include <winsock.h>
+#include <winsock2.h>
+#include <WS2tcpip.h>
+
+int main(void) {
+
+
+	return 0;
+}
