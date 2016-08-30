c29d7ed42bdc16cb648bf622fe3df35bf95af94a
zxchs00 <zxchs00@gmail.com>
2016-08-30 19:45:37 +0900

    my IP and MAC and gateway IP

--- mal_site.c
+++ mal_site.c
@@ -14,7 +14,6 @@
 int my_MACnIP(unsigned char* mac, unsigned char* ipadd) {
 	PIP_ADAPTER_INFO info, pinfo=NULL;
 	DWORD size = sizeof(info);
-	char *mac_addr = (char*)malloc(17);
 	int success=0;
 	int chk, cnt;
 
@@ -56,6 +55,49 @@ int my_MACnIP(unsigned char* mac, unsigned char* ipadd) {
 	return success;	// success 1 , fail 0
 }
 
+int gateway_IP(unsigned char* gip) {
+	PIP_ADAPTER_INFO info, pinfo = NULL;
+	DWORD size = sizeof(info);
+	int success = 0;
+	int chk, cnt;
+
+	info = (IP_ADAPTER_INFO *)malloc(sizeof(IP_ADAPTER_INFO));
+	if (info == NULL) {
+		printf("Error in allocating info\n");
+	}
+
+	if (GetAdaptersInfo(info, &size) == ERROR_BUFFER_OVERFLOW) {
+		info = (IP_ADAPTER_INFO *)malloc(size);
+		if (info == NULL) {
+			printf("Error in allocating info\n");
+		}
+	}
+	if (GetAdaptersInfo(info, &size) == NO_ERROR) {
+		pinfo = info;
+		if (pinfo) {
+			success = 1;
+		}
+	}
+	if (success) {
+		cnt = 0;
+		chk = 0;
+		for (int i = 0; i<strlen(pinfo->GatewayList.IpAddress.String); i++) {
+			if (pinfo->GatewayList.IpAddress.String[i] == '.') {
+				gip[cnt] = chk;
+				chk = 0;
+				cnt++;
+			}
+			else {
+				chk = chk * 10 + pinfo->GatewayList.IpAddress.String[i] - 0x30;
+			}
+		}
+		gip[cnt] = chk;
+	}
+
+	free(info);
+	return success;	// success 1 , fail 0
+}
+
 int main(int argc, char* argv[]) {
 	FILE* fp;
 	char* block[100][100];
@@ -70,11 +112,12 @@ int main(int argc, char* argv[]) {
 	fclose(fp);
 
 	my_MACnIP(&req_data[0], &req_data[6]);
+	gateway_IP(&req_data[10]);
 	for (i = 0; i < 6; i++) {
 		printf("%02x ", req_data[i]);
 	}
 	printf("\n");
-	for (i = 0; i < 4; i++) {
+	for (i = 0; i < 8; i++) {
 		printf("%d.", req_data[6 + i]);
 	}
 	printf("\n");
